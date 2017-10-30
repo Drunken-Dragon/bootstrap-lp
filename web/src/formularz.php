85afbb5fbc885f18be6d2b8c18677ebcb2736749
Jan Zioło <janziolo@Jans-MacBook-Air.local>
2017-10-30 09:50:34 +0100

    created htaccess file that contains mode rewrite and some rewrite rules - removing .php file extenstion in the web browser URL field. Also created web/ directory, which contains index.php as well as .css and img files and created src/ directory, that contains all the rest of the files

---
+++ web/src/formularz.php
@@ -0,0 +1,75 @@
+<?php
+    session_start();
+
+    $csrf = $_SESSION['csrf'];
+?>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Form handling</title>
+  </head>
+    <body>
+
+<?php
+    $email_error = "";
+    $empty_name = $empty_email = $empty_phone = "";
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if (empty($_POST["name"])) {
+            $empty_name = "Please enter your name";
+        } else {
+            $name = sanitize_input($_POST['name']);
+        }
+        if (empty($_POST["email"])) {
+            $empty_email = "Please enter your email address";
+        } else {
+            $email = sanitize_input($_POST['email']);
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $email_error = " Invalid email format";}
+        }
+        if (empty($_POST["phone"])) {
+            $empty_phone = "Please enter your phone number";
+        } else {
+            $phone = sanitize_input($_POST['phone']);
+        }
+    }
+
+    function sanitize_input($data){
+      return htmlspecialchars($data);
+    }
+
+    if (hash_equals($csrf, $_POST['csrf'])) {
+        echo 'CSRF token is valid';
+    } else {
+        die('CSRF token is invalid');
+    }
+?>
+    <p><strong><?= $empty_name;?></strong></p><br>
+    <p><strong><?= $empty_email;?></strong>
+      <strong><?= $email_error;?></strong></p><br>
+    <p><strong><?= $empty_phone;?></strong></p><br>
+
+<?php
+    try
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=landing_form;charset=utf8','root','root');
+        // var_dump($pdo->query('SELECT * FROM form_input')->fetchAll());
+
+        $db_update = $pdo->prepare("INSERT INTO form_input (name, email, phone)
+        VALUES (:name, :email, :phone)");
+        $db_update->bindParam(':name', $name);
+        $db_update->bindParam(':email', $email);
+        $db_update->bindParam(':phone', $phone);
+
+        $db_update->execute();
+
+        echo "New records created successfully";
+    }
+    catch(\Throwable $e)
+    {
+        echo $e->getMessage();
+    }
+ ?>
+    </body>
+</html>
---
+++ web/src/signup.php
@@ -0,0 +1,67 @@
+<?php
+    $msg = "";
+
+    if (isset($_POST['submit']))
+    {
+        $name = $_POST['name'];
+        $password = $_POST['password'];
+        $cPassword = $_POST['cPassword'];
+
+        if ($password != $cPassword)
+            $msg = "Please check your password!";
+        else
+        {
+            $hash = password_hash($password, PASSWORD_BCRYPT);
+
+            try
+            {
+                $pdo = new PDO('mysql:host=localhost;dbname=landing_form_2;charset=utf8','root','root');
+
+                $db_update = $pdo->prepare("INSERT INTO users (name, password)
+                VALUES (:name, :password)");
+                $db_update->bindParam(':name', $name);
+                $db_update->bindParam(':password', $hash);
+
+                $db_update->execute();
+
+                $msg = "You have successfully signed up!";
+            }
+            catch(\Throwable $e)
+            {
+                $msg = $e->getMessage();
+            }
+        }
+    }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container" style="margin-top: 100px">
+        <div class="row justify-content-center">
+            <div class="col-md-6" align="center">
+                <h2>Please sign up</h2>
+
+                <?php if ($msg != "") echo $msg . "<br><br>";?>
+
+                <form method="post" action="signup.php">
+                    <input class="form-control" type="text" name="name" placeholder="User name"><br>
+                    <input class="form-control" type="password" name="password" minlength="8" placeholder="User password"><br>
+                    <input class="form-control" type="password" name="cPassword" minlength="8" placeholder="Confirm User password"><br>
+                    <input class="btn btn-primary" type="submit" name="submit" value="Sign up"><br><br>
+                </form>
+                <form action="../src/login.php">
+                    <input class="btn btn-primary" type="submit" name="submit" value="Go to sign in"><br>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
